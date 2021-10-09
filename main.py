1c15e8e2086c6303424a6d1a9fcbabf24c2d8bc1
narendevil <72681599+narendevil@users.noreply.github.com>
2021-10-09 22:18:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import kivy as K
+from kivy import *
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class MainWidget(Widget):
+    pass
+
+class SAASC(App):
+    pass
+
+SAASC().run()
+
